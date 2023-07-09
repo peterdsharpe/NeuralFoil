778e877e4a7ebff30f835ad5ac6f7d784b7b2494
Peter Sharpe <pds@mit.edu>
2023-07-08 22:28:10 -0400

    Add benchmarks, and improve coefficients based on new data

--- benchmarking/neuralfoil_benchmark.py
+++ benchmarking/neuralfoil_benchmark.py
@@ -1,20 +1,49 @@
 import aerosandbox as asb
 import aerosandbox.numpy as np
+from aerosandbox.tools.code_benchmarking import time_function
 from neuralfoil import get_aero_from_airfoil
 
 airfoil = asb.Airfoil("dae11")
-
 airfoil = airfoil.repanel().normalize()
-
-alpha = np.linspace(-15, 15, 100)
 Re = 1e6
 
-aeros = {}
-
 for model_size in ["xxsmall", "xsmall", "small", "medium", "large", "xlarge", "xxlarge", "xxxlarge"]:
-    aeros[f"NF-{model_size}"] = get_aero_from_airfoil(
+
+    n_runs = 1
+
+
+    def func():
+        alpha = np.linspace(0, 10, n_runs)
+        get_aero_from_airfoil(
+            airfoil=airfoil,
+            alpha=alpha,
+            Re=Re,
+            model_size=model_size
+        )
+
+
+    time_total, _ = time_function(func)
+    print(model_size, time_total)
+
+def func():
+    alpha = np.linspace(0, 10, 1)
+    xf = asb.XFoil(
+        airfoil=airfoil,
+        Re=Re
+    ).alpha(alpha)
+    
+time_total, _ = time_function(func)
+print("XFoil", time_total)
+
+from neuralfoil.CL_linear_regression import get_CL
+
+def func():
+    alpha = np.linspace(0, 10, 1)
+    get_CL(
         airfoil=airfoil,
         alpha=alpha,
-        Re=Re,
-        model_size=model_size
+        Re=Re
     )
+
+time_total, _ = time_function(func)
+print("CL Regression", time_total)
