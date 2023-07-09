5203e377d8b0f5e1af8b89e76046f5f9eb2e561a
Peter Sharpe <pds@mit.edu>
2023-07-08 21:31:38 -0400

    add imports; start benchmarking

---
+++ benchmarking/neuralfoil_benchmark.py
@@ -0,0 +1,3 @@
+import aerosandbox as asb
+import aerosandbox.numpy as np
+from neuralfoil import get_aero_from_airfoil
---
+++ neuralfoil/__init__.py
@@ -0,0 +1,6 @@
+from .neuralfoil import (
+    get_aero_from_kulfan_parameters,
+    get_aero_from_airfoil,
+    get_aero_from_coordinates,
+    get_aero_from_dat_file
+)
