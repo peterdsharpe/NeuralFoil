255e7fcec782104094c7b33f7788f6dc49b9e8e2
Peter Sharpe <pds@mit.edu>
2023-07-17 12:36:06 -0400

    increment required ASB version

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     python_requires='>=3.7',
     install_requires=[
         'numpy >= 1',
-        'aerosandbox >= 4.0.10'
+        'aerosandbox >= 4.0.11'
     ],
     extras_require={
         "training": [
