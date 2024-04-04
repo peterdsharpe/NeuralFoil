d37ac2ca0b0325177842f3a92510e8b5445c4fda
Peter Sharpe <pds@mit.edu>
2024-04-04 11:42:20 -0400

    update logic to use only first 10000 samples for speed. Clean up text.

--- training/gen2_architecture/optimal_sensor_placement/compute_optimal_sensor_placement.py
+++ training/gen2_architecture/optimal_sensor_placement/compute_optimal_sensor_placement.py
@@ -2,10 +2,11 @@ from load_data import df, Data
 import numpy as np
 
 df = df.to_pandas().dropna()
+df = df.sample(10000)
 
 data = np.stack([
-    # (df[f"{side}_bl_theta_{i}"])
-    (df[f"{side}_bl_H_{i}"])
+    (df[f"{side}_bl_theta_{i}"])
+    # (df[f"{side}_bl_H_{i}"])
     for side in [
         "upper",
         "lower",
@@ -14,8 +15,8 @@ data = np.stack([
 ], axis=1)
 
 data_flipped = np.stack([
-    # (df[f"{side}_bl_theta_{i}"])
-    (df[f"{side}_bl_H_{i}"])
+    (df[f"{side}_bl_theta_{i}"])
+    # (df[f"{side}_bl_H_{i}"])
     for side in [
         "lower",
         "upper",
@@ -46,41 +47,54 @@ import aerosandbox.numpy as np
 import matplotlib.pyplot as plt
 import aerosandbox.tools.pretty_plots as p
 
-fig, ax = plt.subplots(figsize=(6, 3))
+fig, ax = plt.subplots(figsize=(4, 1.3))
 
 af = asb.KulfanAirfoil("naca0012")
-af.draw(show=False)
+# af.draw(show=False)
+plt.fill(
+    af.x(),
+    af.y(),
+    facecolor=(0, 0, 0, 0.2),
+    linewidth=1,
+    edgecolor=(0, 0, 0, 0.7)
+)
+
 for i in sensor_ids:
     if i < Data.N:
         x = np.linspace(0, 1, Data.N)[i]
-        plt.plot(*af.upper_coordinates(x).T, ".k", markersize=10)
+        plt.plot(*af.upper_coordinates(x).T, ".k", markersize=9, markeredgewidth=0, alpha=0.9)
     else:
         x = np.linspace(0, 1, Data.N)[i - Data.N]
-        plt.plot(*af.lower_coordinates(x).T, ".k", markersize=10)
+        plt.plot(*af.lower_coordinates(x).T, ".k", markersize=9, markeredgewidth=0, alpha=0.9)
 
-subtitle = "Question: \"If I have 64 sensors, where should I put them on a generic airfoil to reconstruct $H(s)$ with minimum error?\""
-import textwrap
+# subtitle = "Question: \"If I have 64 sensors, where should I put them on a generic airfoil to reconstruct $\\theta(s)$ with minimum error?\""
+# import textwrap
+#
+# plt.text(  # Top center of axes, textwrapped
+#     0.5, 0.95,
+#     textwrap.fill(subtitle, width=65),
+#     horizontalalignment='center',
+#     verticalalignment='top',
+#     transform=plt.gca().transAxes,
+#     fontsize=10, alpha=0.7,
+# )
 
-plt.text( # Top center of axes, textwrapped
-    0.5, 0.95,
-    textwrap.fill(subtitle, width=65),
-    horizontalalignment='center',
-    verticalalignment='top',
-    transform=plt.gca().transAxes,
-    fontsize=11, alpha=0.7,
-)
+# answer = "Answer: \"Roughly evenly across the airfoil.\""
 
-answer = "Answer: \"Roughly evenly across the airfoil.\""
-
-plt.text(
-    0.5, 0.05,
-    textwrap.fill(answer, width=65),
-    horizontalalignment='center',
-    verticalalignment='bottom',
-    transform=plt.gca().transAxes,
-    fontsize=11, alpha=0.7,
-)
+# plt.text(
+#     0.5, 0.05,
+#     textwrap.fill(answer, width=65),
+#     horizontalalignment='center',
+#     verticalalignment='bottom',
+#     transform=plt.gca().transAxes,
+#     fontsize=10, alpha=0.7,
+# )
+plt.axis('off')
+plt.axis('equal')
 
 p.show_plot(
-    "Optimal Sensor Locations for Compressed\nSensing of Shape Factor"
+    # "Optimal Sensor Locations for Compressed\nSensing of Shape Factor $H(s)$",
+    "Optimal Sensor Locations for Compressed\nSensing of Momentum Thickness $\\theta(s)$",
+    # savefig="optimal_sensors_H.svg",
+    savefig="optimal_sensors_theta.svg",
 )
