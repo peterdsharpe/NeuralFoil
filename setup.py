a964518b5f7fe1004be008a5d9dc7c84ea1b8477
Peter Sharpe <pds@mit.edu>
2024-08-17 15:30:07 -0400

    Adds AeroSandbox version pin to allow future synchronized updates

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     python_requires='>=3.7',
     install_requires=[
         'numpy >= 1',
-        'aerosandbox >= 4.2.4'
+        'aerosandbox >= 4.2.4, <4.3.0'
     ],
     extras_require={
         "training": [
